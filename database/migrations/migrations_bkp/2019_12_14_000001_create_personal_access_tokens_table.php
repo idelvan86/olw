01c34c9ad1b7d2449c4a4c57b5d7aac30d649fa6
Isidorio Silva <idelvan.isidorio@hotmail.com>
2023-02-27 17:15:19 -0300

    instalando o limewire

--- database/migrations/migrations_bkp/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/migrations_bkp/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sales', function (Blueprint $table) {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('seller_id')->constrained();
-            $table->foreignId('client_id')->constrained();
-            $table->timestamps('sold_at');
-            $table->char('status',1);
-            $table->integer('total_amount');
+            $table->morphs('tokenable');
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sales');
+        Schema::dropIfExists('personal_access_tokens');
     }
 };
---
+++ git.php
@@ -0,0 +1,9 @@
+/*
+
+Git comandos de log 
+git log
+git log --decorate (informa se houve merge)
+git log --author="idelvan" (Busca commit pelo nome do author)
+git shortlog (mostra os autohores qts commit fez e quals foram os commits (descrição dos commits) )
+git shortlog -sn ( visualiza apenas a qtd de commits e o nome de quem fez )
+git log --graph (mostra em forma grafica o q esta acontecendo com o projeto "merges feitas, branchs mudadas etc")
